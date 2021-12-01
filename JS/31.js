b3aa95c60ca70bef9cfa03154a8c74adb2052074
Omar <omarchavezgutierrezdevelasco@gmail.com>
2021-11-30 23:39:29 -0600

    API notification

--- JS/31.js
+++ JS/31.js
@@ -1,3 +1,9 @@
 // API notification. //
-
+//.querySelector - es la forma para seleccionar contenido HTML.
 const boton = document.querySelector("#boton");
+
+boton.addEventListener("click", () => {
+  Notification.requestPermission().then((resultado) =>
+    console.log("El resultado es: ", resultado)
+  );
+}); //funcion que nos permite registrar un evento a la variable boton que tiene agregado el html con id de boton.
