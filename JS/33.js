f0107a1a48d11ef854d83cd4e459cb0ec2c084e8
Omar <omarchavezgutierrezdevelasco@gmail.com>
2021-12-01 19:53:19 -0600

    fetch API

---
+++ JS/33.js
@@ -0,0 +1,14 @@
+async function obtenerEmpleados() {
+  const archivo = "empleados.json";
+
+  //   fetch(archivo)
+  //     .then((resultado) => resultado.json())
+  //     .then((datos) => {
+  //       const { empleados } = datos;
+  //       console.log(empleados);
+  //     });
+  const resultado = await fetch(archivo);
+  const datos = await resultado.json();
+  console.log(datos);
+}
+obtenerEmpleados();
