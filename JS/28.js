b7abc456ce6b83a4604d90b5df50093590c6cd05
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-01-05 23:12:54 -0600

    cambios de nombre

--- JS/28.js
+++ JS/28.js
@@ -5,20 +5,30 @@ class Producto {
     this.nombre = nombre;
     this.precio = precio;
   }
+
   formatearProducto() {
     return `El producto ${this.nombre} tiene un precio de ${this.precio}`;
+  }
+
+  aumentarPrecio() {
+    const precioNuevo = this.precio + 100;
+    return `tu producto tiene un precio de ${precioNuevo}`;
   }
 }
 
 const Producto2 = new Producto(`monitor curvo de 49"`, 800);
 const Producto3 = new Producto(`laptop`, 300);
 
+console.log(Producto2.formatearProducto());
+
 //Herencia
 // se heredara las propiedades de la funcion que se le indique con 'extends'
 
 class Libro extends Producto {}
-const Libro = new Libro("JavaScript la Revolucion", 120, "986451320653");
-console.log(Libro);
+
+const libro = new Libro("JavaScript la Revolucion", 120, "986451320653");
+
+console.log(libro);
 
 console.log(Producto2);
 console.log(Producto3);
