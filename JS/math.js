15628506e496ef8f9febef5edc4dcd8aa15a46f6
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-01-05 23:13:06 -0600

    math

---
+++ JS/math.js
@@ -0,0 +1,12 @@
+//objeto math
+
+let result;
+result = Math.PI;
+result = Math.round(2.4); //redondea de 2.5 hacia arriba
+result = Math.ceil(4.1); //Ceil siempre redondea hacia arriba
+result = Math.floor(4.9); //floor siempre redondea hacia abajo
+result = Math.sqrt(144); //sqrt nos indica que sacara la raiz cuadrada
+result = Math.abs(-200); //convierte los numeros negativos en positivos
+result = Math.min(3, 5, 2, 8, 4, 11); // nos indica cual es el numero de menor valor de la lista
+result = Math.max(3, 5, 2, 8, 4, 11); // nos indica cual es el numero de mayor valor de la list
+console.log(result);
