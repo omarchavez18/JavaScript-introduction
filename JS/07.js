e27e64df8fbfffa0cac533c0c548cd1fc793343e
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-01-05 23:16:37 -0600

    orden de las operaciones

--- JS/07.js
+++ JS/07.js
@@ -7,5 +7,5 @@ let puntaje = 10;
 //incremento en 1 seria ++
 puntaje++; //1
 //incremento de numero asignado
-puntaje += 12; //aqui seria 11 del resultado anterior mas 12 igual a 2
+puntaje += 12; //aqui seria 11 del resultado anterior mas 12 igual a 23
 console.log(puntaje);
